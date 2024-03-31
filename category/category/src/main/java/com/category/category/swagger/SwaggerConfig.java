d766b68413ae2aed360c17e01ac96387e6be17d3
Srinu-rj <dnsrinu143@gmail.com>
2024-03-31 14:19:31 +0530

    SwaggerConfig added to test APIS

---
+++ category/category/src/main/java/com/category/category/swagger/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package com.category.category.swagger;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+	@Bean
+	public OpenAPI springShopOpenAPI() {
+		return new OpenAPI()
+				.info(new Info().title("Restaurant Application")
+						.description("Backend APIs for E-Commerce app")
+						.version("v1.0.0").contact(new Contact().name("srinu")
+								.url("https://github.com/Srinu-rj")
+								.email("dnsrinu143@gmail.com"))
+						.summary("9642266933")
+						.license(new License().name("License")
+								.url("/"))).externalDocs(new ExternalDocumentation()
+						.description("E-Commerce App Documentation")
+						.url("http://localhost:7007/swagger-ui/index.html"));
+	}
+
+
+}
